cd36abcad2b602d51c8086701bce8059ac2bd474
clayton pereira <cpereiramt@gmail.com>
2021-02-17 12:13:14 -0300

    saving project structure

---
+++ src/main/java/com/claytonpereira/agendaJPA/AgendaJpaApplication.java
@@ -0,0 +1,13 @@
+package com.claytonpereira.agendaJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaJpaApplication.class, args);
+	}
+
+}
