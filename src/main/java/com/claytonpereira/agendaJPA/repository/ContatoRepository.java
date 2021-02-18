a8d892114a14fef34e01a044e6c7247d0bff1eb5
cpereiramt <cpereiramt@gmail.com>
2021-02-17 20:19:43 -0400

    saving changes in project structure

--- src/main/java/com/claytonpereira/agendaJPA/repository/ContatoRepository.java
+++ src/main/java/com/claytonpereira/agendaJPA/repository/ContatoRepository.java
@@ -1,13 +1,7 @@
 package com.claytonpereira.agendaJPA.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.claytonpereira.agendaJPA.model.Contato;
 
 @Repository
--- src/main/java/com/claytonpereira/agendaJPA/resource/ContatoResource.java
+++ src/main/java/com/claytonpereira/agendaJPA/resource/ContatoResource.java
@@ -1,10 +1,14 @@
 package com.claytonpereira.agendaJPA.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +22,7 @@ public class ContatoResource {
 	
 	  @Autowired
 	  ContatoRepository contatoRepository;
+	  
 	  @GetMapping 
 	  public List<Contato> getContatosResource() {
 	   return contatoRepository.findAll();
@@ -25,7 +30,24 @@ public class ContatoResource {
 	 
 	  @PostMapping
 	  public void insertContatoResource(@RequestBody Contato contato) {
-	   contatoRepository.save(contato);
+	   contatoRepository.save(contato);		  
+	  }
+	  
+	  @DeleteMapping("/{id}")
+	  public void deleteContatoResource(@PathVariable int id) {
+		  contatoRepository.deleteById(id);
+	  }
+	  
+	  @PutMapping("/{id}")
+	  public void editContatoResource(@PathVariable int id, @RequestBody Contato contato) {
+		  Optional<Contato> contatoToUpdate = contatoRepository.findById(id);
+		  if (contatoToUpdate.isPresent()) {
+			  contatoToUpdate.get().setNome(contato.getNome());
+			  contatoToUpdate.get().setEndereco(contato.getEndereco());
+			  contatoToUpdate.get().setTelefone(contato.getTelefone());
+			  contatoToUpdate.get().setDataNascimento(contato.getDataNascimento());
+			  contatoRepository.save(contatoToUpdate.get());
+		  }
 		  
 	  }
 }
